ae576b4433a58dbf0926e52faeb07d0ce5cfa48c
singhjagjit11 <116024713+singhjagjit11@users.noreply.github.com>
2023-04-19 18:40:30 -0400

    Add files via upload

---
+++ assign/modules/rgroup/main.tf
@@ -0,0 +1,14 @@
+
+	
+resource "azurerm_resource_group" "rgroup" {
+  name     = "N01574661-assignment1-RG"
+  location = "eastus"
+  
+  tags = {
+    Environment    = "Lab"
+    ExpirationDate = "2023-06-30"
+    Name           = "jagjit.singh"
+    Project        = "Automation Project – Assignment 1"
+  }
+}
+
